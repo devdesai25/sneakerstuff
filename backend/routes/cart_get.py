a1f457d1e8fe9247e1e7fa217d909a5ad9f74a5b
devdesai25 <desaidev1610@gmail.com>
2026-06-02 13:28:13 +0530

    Added route to retrive cart items

---
+++ backend/routes/cart_get.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, Depends
+from database import get_db
+from schemas.cart_items import CartResponse
+from sqlalchemy.orm import Session
+from models.cart_items import CartItem
+from services.auth import get_current_user
+from models.users import User
+
+router = APIRouter(
+    tags=["userCart"])
+
+@router.get("/cart", response_model= list[CartResponse])
+def get_cart(
+    db: Session = Depends(get_db), 
+    user: User = Depends(get_current_user)
+):
+    
+    cart = db.query(CartItem).filter(CartItem.user_id == user.id).all()
+    
+    return cart
